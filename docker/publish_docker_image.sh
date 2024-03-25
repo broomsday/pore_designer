cacf1699e65a936f0ef3c8fa16dde443e8ad86d0
Aron Broom <broomsday@gmail.com>
2024-03-25 16:35:49 -0400

    Fix typo in docker publishing

--- docker/publish_docker_image.sh
+++ docker/publish_docker_image.sh
@@ -1,3 +1,3 @@
 sudo docker login
-sudo docker tag pore_designer:latest antiquatedarachnid:pore_designer:latest
-sudo docker push antiquatedarachnid:latest
+sudo docker tag pore_designer:latest antiquatedarachnid/pore_designer:latest
+sudo docker push antiquatedarachnid/pore_designer:latest
