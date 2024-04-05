321bfd42d80f30f32ef32a4f70e60764ededbf42
Aron Broom <broomsday@gmail.com>
2024-04-05 13:07:39 -0400

    Update docker and move install scripts into another repo

--- scripts/fix_alphafold.sh
+++
@@ -1,8 +0,0 @@
-COLABFOLDDIR="/AlphaFold/localcolabfold/"
-ALPHAFOLD_ENV="/AlphaFold/localcolabfold/colabfold-conda"
-
-source "${COLABFOLDDIR}/conda/etc/profile.d/conda.sh"
-export PATH="${COLABFOLDDIR}/conda/condabin:${PATH}"
-conda activate "$COLABFOLDDIR/colabfold-conda"
-"$COLABFOLDDIR/colabfold-conda/bin/pip" install "tensorflow[and-cuda]==2.14"
-conda deactivate
