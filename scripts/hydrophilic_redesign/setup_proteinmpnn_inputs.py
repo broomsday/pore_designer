cf1cacd7c2e9301c797549a77b6ae82b04816178
Aron Broom <broomsday@gmail.com>
2025-06-03 15:17:01 -0400

    Correct fixed positions bug

--- scripts/hydrophilic_redesign/setup_proteinmpnn_inputs.py
+++ scripts/hydrophilic_redesign/setup_proteinmpnn_inputs.py
@@ -8,14 +8,14 @@ import typer
 import biotite.structure as bts
 from biotite.structure.io import save_structure
 from designer.proteinmpnn import make_symmetry_dict, make_aa_bias_dict, make_symmetric_fixed_positions_dict, save_proteinmpnn_jsonl_dict, rekey_proteinmpnn_dict
-from designer.pdb import clean_pdb
+from designer.pdb import clean_pdb, get_sequence
 
 
 def generate_proteinmpnn_manual_run_command(input_dir: Path, pdb_name: str, num_designs: int, fixed_positions: bool, biased_aas: bool, output_dir: Path) -> str:
     """
     Generate the string that would be used to run ProteinMPNN for hydrophilic redesign.
     """
-    command = f"protein_mpnn_run.py --use_soluble_model --num_seq_per_target {num_designs} --pdb_path {(input_dir / pdb_name).absolute()}"
+    command = f"python protein_mpnn_run.py --use_soluble_model --num_seq_per_target {num_designs} --pdb_path {(input_dir / pdb_name).absolute()}"
     command += f" --tied_positions_jsonl {(input_dir / 'tied_positions.jsonl').absolute()} --out_folder {output_dir.absolute()}"
 
     if fixed_positions:
@@ -24,16 +24,18 @@ def generate_proteinmpnn_manual_run_command(input_dir: Path, pdb_name: str, num_
     if biased_aas:
         command += f" --bias_AA_jsonl {(input_dir / 'biased_aas.jsonl').absolute()}"
 
+    command += " --sampling_temp 0.2"
+
     return command
 
 
 def main(
         input_pdb: Path = typer.Argument(..., help="PDB that will be designed."),
         setup_dir: Path = typer.Argument(..., help="Where to copy the PDB, and save all the jsonl files."),
-        fixed_positions_string: str = typer.Option(default="3,7,9,14,18", help="List of positions to fix in each chain"),
-        biased_aas_string: str = typer.Option(default="V,I,L,M", help="List of amino acids to bias against"),
-        bias_strength: float = typer.Option(default=-1.0, help="Strength of the bias, -ve reduces odds of design"),
-        num_designs: int = typer.Option(default=10000),
+        mutable_positions_string: str = typer.Option(default="3,7,10,14,18", help="List of positions to fix in each chain"),
+        biased_aas_string: str = typer.Option(default="V,I,L,M,F", help="List of amino acids to bias against"),
+        bias_strength: float = typer.Option(default=-1.5, help="Strength of the bias, -ve reduces odds of design"),
+        num_designs: int = typer.Option(default=100),
     ):
     """
     Generate inputs needed to run ProteinMPNN for hydrophilic redesign.
@@ -51,10 +53,15 @@ def main(
     save_proteinmpnn_jsonl_dict(tied_positions_dict, input_dir / "tied_positions.jsonl")
     rekey_proteinmpnn_dict(input_dir / "tied_positions.jsonl")
 
-    if fixed_positions_string:
-        chains = [chain.chain_id[0] for chain in bts.chain_iter(structure)]
-        fixed_positions = [int(position.strip()) for position in fixed_positions_string.split(",")]
-        fixed_positions_dict = make_symmetric_fixed_positions_dict(input_pdb.stem, chains, fixed_positions)
+    if mutable_positions_string:
+        chain_lengths = [len(get_sequence(chain, mode="single")) for chain in bts.chain_iter(structure)]
+        if len(set(chain_lengths)) != 1:
+            raise NotImplementedError("Does not work for inputs with chains of differing lengths")
+
+        chain_ids = [chain.chain_id[0] for chain in bts.chain_iter(structure)]
+        mutable_positions = [int(position.strip()) for position in mutable_positions_string.split(",")]
+        fixed_positions = [i for i in range(1, chain_lengths[0] + 1) if i not in mutable_positions]
+        fixed_positions_dict = make_symmetric_fixed_positions_dict(input_pdb.stem, chain_ids, fixed_positions)
         save_proteinmpnn_jsonl_dict(fixed_positions_dict, input_dir / "fixed_positions.jsonl")
         rekey_proteinmpnn_dict(input_dir / "fixed_positions.jsonl")
 
@@ -64,7 +71,7 @@ def main(
         save_proteinmpnn_jsonl_dict(aa_bias_dict, input_dir / "biased_aas.jsonl")
 
 
-    print(generate_proteinmpnn_manual_run_command(input_dir, "input.pdb", num_designs, bool(fixed_positions_string), bool(biased_aas_string), output_dir))
+    print(generate_proteinmpnn_manual_run_command(input_dir, "input.pdb", num_designs, bool(mutable_positions_string), bool(biased_aas_string), output_dir))
 
 
 if __name__ == "__main__":
